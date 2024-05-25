bbe4bce688bd5b8a228ea0021dac879b78f4ceda
Sriram R <sriramrk9495@gmail.com>
2024-05-25 13:33:54 +0530

    tailwind-landing-page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      colors: {
+        customPink: '#F26496',
+      },
+    },
+  },
+  plugins: [],
+}
+
